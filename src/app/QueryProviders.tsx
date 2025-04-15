7ec517255a9eadf56bebbfaac066a84d794d83c8
Namgyeon <getam101@naver.com>
2025-04-15 20:42:54 +0900

    feat: 랜딩페이지 nav 구현

--- src/app/QueryProviders.tsx
+++ src/app/QueryProviders.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import Nav from "@/components/Nav";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 const queryClient = new QueryClient();
 
 export default function QueryProviders({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+  const signHideLayout = pathname === "/signin" || pathname === "/signup";
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {!signHideLayout && <Nav />}
+      {children}
+    </QueryClientProvider>
   );
 }
