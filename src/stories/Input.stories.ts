d66f88def30b26c303fdc95c081a4dbb9dfb07ae
Namgyeon <getam101@naver.com>
2025-05-20 16:31:42 +0900

    feat: PasswordInput 스토리북 추가

--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -5,14 +5,14 @@ export default {
   component: Input,
 };
 
-export const OriginInput = {
+export const Basic = {
   args: {
     label: "아이디",
     placeholder: "아이디를 입력해주세요.",
   },
 };
 
-export const ErrorInput = {
+export const ErrorState = {
   args: {
     label: "아이디",
     placeholder: "아이디를 입력해주세요.",
