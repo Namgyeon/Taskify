90dd860114f91bd19e0882c9c9169c10c89778b8
Namgyeon <getam101@naver.com>
2025-05-21 16:24:32 +0900

    feat: 메인랜딩페이지 hero컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-"use client";
-
-import apiClient from "@/lib/api/apiClient";
-import { useEffect } from "react";
+import Hero from "../components/landing/Hero";
 
 export default function Home() {
-  useEffect(() => {
-    const textGetMy = async () => {
-      try {
-        const res = await apiClient.get("/users/me");
-        console.log(res.data);
-      } catch (err) {
-        console.error("실패:", err);
-      }
-    };
-
-    textGetMy();
-  }, []);
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>홈페이지</p>
+    <div className="w-full min-h-screen flex flex-col bg-black">
+      <Hero />
     </div>
   );
 }
