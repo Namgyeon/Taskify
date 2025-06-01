6b90853c00d3f3e05754298ca791b8ee009859fe
Namgyeon <getam101@naver.com>
2025-06-01 22:31:44 +0900

    feat: 대시보다 헤더 임시 구현

--- src/components/mydashboard/header/header.tsx
+++ src/components/mydashboard/header/header.tsx
@@ -1,16 +1,15 @@
 "use client";
 
+import DashBoardInfo from "./DashBoardInfo";
 import Profile from "./Profile";
 
 const Header = () => {
   return (
     <div className="flex justify-between items-center px-3 md:px-8 lg:px-10 py-4">
       <p className="text-lg md:text-xl text-[#333236] font-bold">내 대시보드</p>
-      <div className="flex">
-        <div className="flex">
-          {/* 따로 컴포넌트 만들어서 구현
-          <button>관리</button>
-          <button>초대하기</button> */}
+      <div className="flex gap-4 md:gap-9 divide-x divide-gray-300">
+        <div className="flex pr-4 md:pr-9 items-center">
+          <DashBoardInfo />
         </div>
         <div className="flex">
           <Profile />
