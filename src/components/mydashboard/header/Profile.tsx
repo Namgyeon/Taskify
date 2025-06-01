4b8f6c207a0f0a6fc59e4b1fc3ab18b58b15697d
Namgyeon <getam101@naver.com>
2025-06-01 18:49:41 +0900

    feat: 헤더 프로필 구현

--- src/components/mydashboard/header/Profile.tsx
+++ src/components/mydashboard/header/Profile.tsx
@@ -6,18 +6,31 @@ const Profile = () => {
   console.log(data);
   return (
     <div>
-      {data && (
-        <div className="flex gap-3 items-center">
-          <Avatar
-            profileImageUrl={data.profileImageUrl}
-            email={data.email}
-            nickname={data.nickname}
-          />
-          <p className="font-medium text-[#333236]">{data.nickname}</p>
-        </div>
+      {isFetching ? (
+        <Skeleton />
+      ) : (
+        data && (
+          <div className="flex gap-3 items-center">
+            <Avatar
+              profileImageUrl={data.profileImageUrl}
+              email={data.email}
+              nickname={data.nickname}
+            />
+            <p className="font-medium text-[#333236]">{data.nickname}</p>
+          </div>
+        )
       )}
     </div>
   );
 };
+
+function Skeleton() {
+  return (
+    <div className="flex gap-3 items-center">
+      <div className="flex aspect-square w-9 rounded-full animate-pulse bg-gray-200" />
+      <span className="block h-4 w-16 rounded-md animate-pulse bg-gray-200" />
+    </div>
+  );
+}
 
 export default Profile;
