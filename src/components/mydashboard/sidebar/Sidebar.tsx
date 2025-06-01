ff7b31da9bc1ef1e2c4ef34692141cc3997addc1
Namgyeon <getam101@naver.com>
2025-06-01 23:39:18 +0900

    feat: 사이드바 헤더 구현

---
+++ src/components/mydashboard/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import SidebarHeader from "./SidebarHeader";
+
+const DashBoardSideBar = () => {
+  return (
+    <div className="h-screen border-r border-r-gray-300">
+      <SidebarHeader />
+    </div>
+  );
+};
+
+export default DashBoardSideBar;
