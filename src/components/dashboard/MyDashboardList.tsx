09ef6f9b6887e71d59a3d1d2addc2d6e0bcf2bd0
Namgyeon <getam101@naver.com>
2025-06-14 22:16:15 +0900

    feat: 대시보드 페이지 목록 페이지네이션 구현

--- src/components/dashboard/MyDashboardList.tsx
+++ src/components/dashboard/MyDashboardList.tsx
@@ -3,24 +3,40 @@
 import { useGetDashboardsQuery } from "@/apis/dashboards/queries";
 import MyDashboardCard from "./MyDashboardCard";
 import CreateDashboard from "./CreateDashboard";
+import Pagination from "../pagination/Pagination";
+import { useState } from "react";
+
+const ITEMS_PER_PAGE = 5;
+// const SKELETON_COUNT = 5;
 
 const MyDashboardList = () => {
+  const [page, setPage] = useState<number>(1);
+
   const { data, isLoading } = useGetDashboardsQuery({
     navigationMethod: "pagination",
-    page: 1,
-    size: 10,
+    page,
+    size: ITEMS_PER_PAGE,
   });
+
+  const totalCount = data?.totalCount ?? 0;
+  const totalPage = Math.ceil(totalCount / ITEMS_PER_PAGE);
 
   if (isLoading) {
     return <div>로딩중</div>;
   }
 
   return (
-    <div className="flex flex-row flex-wrap gap-2 w-full">
-      <CreateDashboard />
-      {data?.dashboards.map((dashboard) => {
-        return <MyDashboardCard key={dashboard.id} dashboard={dashboard} />;
-      })}
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-row flex-wrap gap-2 w-full">
+        <CreateDashboard />
+        {data?.dashboards.map((dashboard) => {
+          return <MyDashboardCard key={dashboard.id} dashboard={dashboard} />;
+        })}
+      </div>
+      <div className="w-full flex justify-start items-center gap-2">
+        <Pagination page={page} totalPage={totalPage} setPage={setPage} />
+        <p>{`${totalPage} 페이지 중 ${page}`}</p>
+      </div>
     </div>
   );
 };
