d417895d04ecc923b6ea76cbb63a9f53dfc15d6a
Namgyeon <getam101@naver.com>
2025-06-16 14:42:20 +0900

    feat: 대시보드 카드에서 클릭시 대시보드 상세페이지 이동 구현

--- src/components/dashboard/MyDashboardCard.tsx
+++ src/components/dashboard/MyDashboardCard.tsx
@@ -1,13 +1,23 @@
 import { Dashboard } from "@/apis/dashboards/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface MyDashboardCardProps {
   dashboard: Dashboard;
 }
 
 const MyDashboardCard = ({ dashboard }: MyDashboardCardProps) => {
+  const router = useRouter();
+
+  const handleDashboardClick = (dashboardId: number) => {
+    router.push(`dashboard/${dashboardId}`);
+  };
+
   return (
-    <div className="w-65 md:w-62 lg:w-83 h-15 flex justify-between items-center py-4 px-5 border border-gray-300  rounded-lg cursor-pointer bg-white hover:bg-gray-300">
+    <div
+      className="w-65 md:w-62 lg:w-83 h-15 flex justify-between items-center py-4 px-5 border border-gray-300  rounded-lg cursor-pointer bg-white hover:bg-gray-300"
+      onClick={() => handleDashboardClick(dashboard.id)}
+    >
       <div className="flex items-center gap-2">
         <div
           className="w-2 h-2 rounded-full flex-shrink-0"
