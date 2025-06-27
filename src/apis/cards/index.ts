d0eeef918703cce8604cf40829b4a80b55c27679
Namgyeon <getam101@naver.com>
2025-06-27 15:50:03 +0900

    feat: 카드리스트 목록 api 구현

--- src/apis/cards/index.ts
+++ src/apis/cards/index.ts
@@ -1,5 +1,12 @@
 import { safeResponse } from "@/utils/network/safeResponse";
-import { Card, cardSchema, CreateCardRequest } from "./types";
+import {
+  Card,
+  CardListRequest,
+  CardListResponse,
+  cardListResponseSchema,
+  cardSchema,
+  CreateCardRequest,
+} from "./types";
 import axiosClientHelper from "@/utils/network/axiosClientHelper";
 
 /**
@@ -9,4 +16,15 @@ import axiosClientHelper from "@/utils/network/axiosClientHelper";
 export const postCard = async (formData: CreateCardRequest) => {
   const response = await axiosClientHelper.post<Card>("/cards", formData);
   return safeResponse(response.data, cardSchema);
+};
+
+/**
+ * 카드 목록 조회
+ * https://sp-taskify-api.vercel.app/docs/#/cards
+ */
+export const getCards = async (params: CardListRequest) => {
+  const response = await axiosClientHelper.get<CardListResponse>("/cards", {
+    params,
+  });
+  return safeResponse(response.data, cardListResponseSchema);
 };
