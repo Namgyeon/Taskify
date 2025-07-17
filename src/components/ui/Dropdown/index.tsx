c50f815e399b57e1f44b951cfc6f4cf0d6ed25d8
Namgyeon <getam101@naver.com>
2025-07-17 13:46:32 +0900

    feat:헤더 프로필을 통해 myPage 이동기능 구현

--- src/components/ui/Dropdown/index.tsx
+++ src/components/ui/Dropdown/index.tsx
@@ -11,14 +11,23 @@ interface DropdownOption {
 
 interface DropdownProps {
   options: DropdownOption[];
+  text?: string;
   icon?: string;
   className?: string;
+  optionClassName?: string;
   value?: string;
   onChange?: (value: string) => void;
   selectedInput?: boolean;
 }
 
-const Dropdown = ({ options, icon, onChange }: DropdownProps) => {
+const Dropdown = ({
+  options,
+  icon,
+  onChange,
+  text,
+  className,
+  optionClassName,
+}: DropdownProps) => {
   const handleOptionClick = (option: DropdownOption) => {
     if (option.onClick) {
       option.onClick();
@@ -28,13 +37,23 @@ const Dropdown = ({ options, icon, onChange }: DropdownProps) => {
 
   return (
     <Menu as="div" className="relative">
-      <Menu.Button className="relative flex items-center w-5 h-5 md:w-7 md:h-7 hover:bg-gray-200 rounded-lg transition-colors cursor-pointer">
-        <Image
-          src={icon ?? "/ui/kebabMenu-icon.svg"}
-          alt="드롭다운 아이콘"
-          fill
-          className="object-cover"
-        />
+      <Menu.Button
+        className={clsx(
+          "relative flex items-center w-5 h-5 md:w-7 md:h-7 hover:bg-gray-200 rounded-lg transition-colors cursor-pointer",
+          className
+        )}
+      >
+        {!text && (
+          <Image
+            src={icon ?? "/ui/kebabMenu-icon.svg"}
+            alt="드롭다운 아이콘"
+            fill
+            className="object-cover"
+          />
+        )}
+        {text && (
+          <p className="md:text-lg font-medium text-[#333236]">{text}</p>
+        )}
       </Menu.Button>
 
       <Menu.Items className="absolute right-0 mt-2 bg-white rounded-lg shadow-lg border border-gray-200 z-[9999]">
@@ -48,7 +67,8 @@ const Dropdown = ({ options, icon, onChange }: DropdownProps) => {
                   "px-4 py-2 rounded-lg whitespace-nowrap cursor-pointer transition-colors text-left w-full",
                   active
                     ? "bg-gray-200 text-purple-500"
-                    : "text-gray-700 hover:bg-gray-100"
+                    : "text-gray-700 hover:bg-gray-100",
+                  optionClassName
                 )}
               >
                 {option.label}
