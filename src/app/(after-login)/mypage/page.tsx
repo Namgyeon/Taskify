cb84c7fc37ea936446091bbb67054b2e65c935a1
Namgyeon <getam101@naver.com>
2025-07-17 13:51:07 +0900

    feat:mypage 뒤로가기 버튼 구현

--- src/app/(after-login)/mypage/page.tsx
+++ src/app/(after-login)/mypage/page.tsx
@@ -1,5 +1,11 @@
+import BackButton from "@/components/edit/BackButton";
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div className="p-5 flex flex-col gap-4">
+      <BackButton href="/mydashboard" />
+    </div>
+  );
 };
 
 export default MyPage;
