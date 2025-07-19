53141ead3ed46bc24306919a553f864a3ffd4573
Namgyeon <getam101@naver.com>
2025-07-19 15:14:13 +0900

    배포오류해결 (#56)

--- src/utils/network/axiosServerHelper.ts
+++ src/utils/network/axiosServerHelper.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
+console.log("Environment API URL:", process.env.NEXT_PUBLIC_API_URL);
+
 const axiosServerHelper = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
