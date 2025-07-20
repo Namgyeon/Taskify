d7b2e274f915dc6e83a16dc37ead0415c0e1ea8c
Namgyeon <getam101@naver.com>
2025-07-20 11:36:33 +0900

    refactor: ColumnTitle 컴포넌트 카드 갯수 동적으로 구현

--- src/components/columns/ColumnTitle.tsx
+++ src/components/columns/ColumnTitle.tsx
@@ -1,7 +1,9 @@
+import { useGetCardsQuery } from "@/apis/cards/queries";
 import ColumnSettingBtn from "./ColumnSettingBtn";
 import { Column } from "@/apis/columns/types";
 
 const ColumnTitle = (column: Column) => {
+  const { data: cardList } = useGetCardsQuery({ columnId: column.id });
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-2">
@@ -10,7 +12,7 @@ const ColumnTitle = (column: Column) => {
           {column.title}
         </p>
         <p className="w-5 h-5 flex items-center justify-center text-xs text-[#787486] rounded-md bg-[#EEEEEE]">
-          2
+          {cardList?.pages[0].totalCount}
         </p>
       </div>
       <div>
