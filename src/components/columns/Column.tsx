19da0a5012f12f525b308b71877d7f188f83a482
Namgyeon <getam101@naver.com>
2025-07-20 15:46:37 +0900

    style: 컬럼 카드리스트 반응형 수정

--- src/components/columns/Column.tsx
+++ src/components/columns/Column.tsx
@@ -9,7 +9,7 @@ interface ColumnProps {
 
 const Column = ({ column }: ColumnProps) => {
   return (
-    <div className="flex flex-col gap-6 min-w-88 px-3 md:px-5 py-4 md:py-6 border-b lg:border-b-0 lg:border-r border-gray-300 overflow-y-auto">
+    <div className="flex flex-col gap-6 min-w-88 px-3 md:px-5 py-4 md:py-6 border-b lg:border-b-0 lg:border-r border-gray-300 lg:overflow-y-auto">
       <ColumnTitle {...column} />
       <CreateCard columnId={column.id} />
       <CardList columnId={column.id} />
