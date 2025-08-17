fabdb7aaf0eb62516024237251038c74979fb22d
Namgyeon <getam101@naver.com>
2025-08-17 18:19:07 +0900

    design:헤더 유저이름 모바일사이즈에서 삭제

--- src/components/dashboard/header/DashBoardInfo.tsx
+++ src/components/dashboard/header/DashBoardInfo.tsx
@@ -25,7 +25,9 @@ const DashBoardInfo = ({ dashboardId }: { dashboardId: number }) => {
             초대하기
           </HeaderButton>
         </div>
-        <AvatarStack dashboardId={dashboardId} />
+        <div className="flex-shrink-0">
+          <AvatarStack dashboardId={dashboardId} />
+        </div>
       </div>
       <Modal ref={modalRef}>
         <InviteModal
